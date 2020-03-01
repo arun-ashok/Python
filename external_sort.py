fef29fa4d8947cc746dc4fd767c1dda8757f802e
Arun Ashok <arunashok22041996@gmail.com>
2020-03-01 09:55:49 +0530

    Splitting input files to 600 mb small files done

--- external_sort.py
+++ external_sort.py
@@ -1,8 +1,28 @@
+import os
+def convert_bytes(num):
+    for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+        if num < 1024.0:
+            return "%3.1f %s" % (num, x)
+        num /= 1024.0
+        
 def ext_sort():
-    fh=open("Input.txt","r");
+    fh=open("temp_words_1.txt","rb",64*1024)
+    i=0
+    file_name="out"+str(i)+".txt"
+    f2=open(file_name,"wb")
     for line in fh:
-        print(line)
+        f2.write(line)
+        file_info = os.stat(file_name)
+        size=convert_bytes(file_info.st_size)
+        l1=size.split(' ')
+        l2=l1[0].split('.')
+        if(int(l2[0])>=300 and l1[1]=='MB'):
+            f2.close()
+            i=i+1            
+            file_name="out"+str(i)+".txt"
+            f2=open(file_name,"wb")
     fh.close()
+    f2.close()
 
 
 if __name__=="__main__":
